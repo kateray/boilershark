1fb10119bf33cea3e8a4edeb9e3b349e2ecdee6a
Kate Ray <kateray@users.noreply.github.com>
2017-07-13 14:34:19 -0400

    Setup Part 3

---
+++ part-3/README.md
@@ -0,0 +1,4 @@
+# Part 3 - Redux & Routing
+
+To run:
+`$ yarn start`
---
+++ part-3/client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+require('../css/App.scss')
+
+render(
+  <div>
+    Hello Sharks!
+  </div>,
+  document.getElementById('root')
+)
